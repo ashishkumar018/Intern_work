d757d960d3345d387f052f9c8092cf44eab31246
ashishkumar018 <ashish@indgeos.in>
2022-12-09 12:18:55 +0530

    added overlay

--- main.js
+++ main.js
@@ -1,41 +1,11 @@
-// import Map from 'ol/Map';
-// import TileLayer from 'ol/layer/Tile';
-// import View from 'ol/View';
-// import XYZ from 'ol/source/XYZ';
-// import {FullScreen, defaults as defaultControls} from 'ol/control';
-
-// const view = new View({
-//   center: [-9101767, 2822912],
-//   zoom: 14,
-// });
-
-// const key = 'FBwl0cAS7xN1ML7lcqpR';
-// const attributions =
-//   '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
-//   '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
-
-// const map = new Map({
-//   controls: defaultControls().extend([new FullScreen()]),
-//   layers: [
-//     new TileLayer({
-//       source: new XYZ({
-//         attributions: attributions,
-//         url:
-//           // 'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=' + key,
-//           'https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=' + key,
-//         maxZoom: 20,
-//       }),
-//     }),
-//   ],
-//   target: 'map',
-//   view: view,
-// });
-
 import KML from 'ol/format/KML';
 import Map from 'ol/Map';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import XYZ from 'ol/source/XYZ';
+import Overlay from 'ol/Overlay';
+import {toLonLat} from 'ol/proj';
+import {toStringHDMS} from 'ol/coordinate';
 import {FullScreen, defaults as defaultControls} from 'ol/control';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 
@@ -43,6 +13,34 @@ const key = 'FBwl0cAS7xN1ML7lcqpR';
 const attributions =
   '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
   '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+
+
+  // Elements that make up the popup.
+
+const container = document.getElementById('popup');
+const content = document.getElementById('popup-content');
+const closer = document.getElementById('popup-closer');
+
+
+// Create an overlay to anchor the popup to the map.
+const overlay = new Overlay({
+  element: container,
+  autoPan: {
+    animation: {
+      duration: 250,
+    },
+  },
+});
+
+// * Add a click handler to hide the popup.
+// * @return {boolean} Don't follow the href.
+closer.onclick = function () {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+
 
 const raster = new TileLayer({
   source: new XYZ({
@@ -64,12 +62,22 @@ const vector = new VectorLayer({
 const map = new Map({
   controls: defaultControls().extend([new FullScreen()]),
   layers: [raster, vector],
+  overlays: [overlay],
   target: document.getElementById('map'),
   view: new View({
     center: [876970.8463461736, 5859807.853963373],
     projection: 'EPSG:3857',
     zoom: 10,
   }),
+});
+
+// Add a click handler to the map to render the popup.
+map.on('singleclick', function (evt) {
+  const coordinate = evt.coordinate;
+  const hdms = toStringHDMS(toLonLat(coordinate));
+
+  content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
+  overlay.setPosition(coordinate);
 });
 
 const displayFeatureInfo = function (pixel) {
